a160c05f3c4aca9909b4893aa817425f6cf416ee
Matt <info@mgates.me>
2021-03-14 19:20:25 -0400

    fix reminder

--- api/reminder.ts
+++ api/reminder.ts
@@ -1,26 +1,37 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-import { connectToDatabase } from "./_connectToDatabase";
-import { sendMsg, StandupGroup, Member, About } from "./_helpers";
+import { NowRequest, NowResponse } from '@vercel/node';
+import { connectToDatabase } from './_connectToDatabase';
+import { sendMsg, StandupGroup, Member, About } from './_helpers';
 
 module.exports = async (req: NowRequest, res: NowResponse) => {
   const db = await connectToDatabase();
-  const groups = await db.collection("groups").find({}).toArray();
+  const groups = await db.collection('groups').find({}).toArray();
 
-
-  const reminders = []
+  const reminders = [];
   groups.forEach((group: StandupGroup) => {
-    var item = group.members[Math.floor(Math.random() * group.members.length)];
-    var items = [item];
+    var validMembers = group.members.filter((m: Member) => m && m.about && m.about.id);
+    var member = validMembers[Math.floor(Math.random() * validMembers.length)];
 
-    items.forEach((member: Member) => {
-      if (member.submitted === false) {
-        reminders.push(sendMsg("Hello, you've been selected! Please submit an update. Update is due by 6pm.", member.about.id))
-      }
-    });
+    if (!member.submitted) {
+      reminders.push(
+        sendMsg(
+          "Hello, you've been selected! Please submit an update. Update is due by 6pm.",
+          member.about.id
+        )
+      );
+      reminders.push(
+        sendMsg(
+          'Na, ez a beszéd!!!!! Next person selected: ' +
+            member.about.first_name +
+            ' (@' +
+            member.about.username +
+            '):',
+          group.chatId
+        )
+      );
+    }
   });
 
-  await Promise.all(reminders)
+  await Promise.all(reminders);
 
   res.json({ status: 200 });
-
 };
--- api/send.ts
+++ api/send.ts
@@ -1,39 +1,54 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-import { connectToDatabase } from "./_connectToDatabase";
-import { sendMsg, StandupGroup, Member, About } from "./_helpers";
-
-
+import { NowRequest, NowResponse } from '@vercel/node';
+import { connectToDatabase } from './_connectToDatabase';
+import { sendMsg, StandupGroup, Member, About } from './_helpers';
 
 module.exports = async (req: NowRequest, res: NowResponse) => {
-
-
   const db = await connectToDatabase();
 
-    const addUpdate = async () => { await db.collection("groups").updateMany(
-    {  },
-    {
-      $set: {
-        "members.$[elem].submitted": false,
-        "members.$[elem].update": '',
+  const addUpdate = async () => {
+    await db.collection('groups').updateMany(
+      {},
+      {
+        $set: {
+          'members.$[elem].submitted': false,
+          'members.$[elem].update': '',
+        },
       },
-    },
-    { arrayFilters: [{ "elem.submitted": true}] }
-  );
-}
-  const groups = await db.collection("groups").find({}).toArray();
+      { arrayFilters: [{ 'elem.submitted': true }] }
+    );
+  };
+  const groups = await db.collection('groups').find({}).toArray();
 
   const sentStandup = [];
   groups.forEach((group: StandupGroup) => {
-    group.members.forEach((member: Member) => {
+    var validMembers = group.members.filter(
+      (m: Member) => m && m.about && m.about.id && m.update
+    );
+    if (!validMembers || !validMembers.length) {
       sentStandup.push(
-        sendMsg("Na, ez a beszéd!!!!! " + member.about.first_name + " (@" + member.about.username + "): \n" + member.update, group.chatId)
+        sendMsg(
+          'Na, ez NEM a beszéd!! Senki irt egy updateot!! ',
+          group.chatId
+        )
       );
-
+    }
+    validMembers.forEach((member: Member) => {
+      sentStandup.push(
+        sendMsg(
+          'Na, ez a beszéd!! ' +
+            member.about.first_name +
+            ' (@' +
+            member.about.username +
+            '): \n' +
+            member.update,
+          group.chatId
+        )
+      );
     });
   });
 
   await Promise.all(sentStandup);
-  await addUpdate()
+  await addUpdate();
 
   res.json({ status: 200 });
 };
