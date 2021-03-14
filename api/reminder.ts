b8437c1551e0ef694b0d82e11c4ee96bb2ed81ad
Matt <info@mgates.me>
2021-03-14 19:35:45 -0400

    fixed

--- api/reminder.ts
+++ api/reminder.ts
@@ -8,27 +8,32 @@ module.exports = async (req: NowRequest, res: NowResponse) => {
 
   const reminders = [];
   groups.forEach((group: StandupGroup) => {
-    var validMembers = group.members.filter((m: Member) => m && m.about && m.about.id);
+    var validMembers = Array.isArray(group.members) && group.members.filter(
+      (m: Member) => m && m.about && m.about.id && !m.submitted
+    ) || [];
+
+    if(!Array.isArray(validMembers) || !validMembers.length) {
+      return;
+    }
+
     var member = validMembers[Math.floor(Math.random() * validMembers.length)];
 
-    if (!member.submitted) {
-      reminders.push(
-        sendMsg(
-          "Hello, you've been selected! Please submit an update. Update is due by 6pm.",
-          member.about.id
-        )
-      );
-      reminders.push(
-        sendMsg(
-          'Na, ez a beszéd!!!!! Next person selected: ' +
-            member.about.first_name +
-            ' (@' +
-            member.about.username +
-            '):',
-          group.chatId
-        )
-      );
-    }
+    reminders.push(
+      sendMsg(
+        "Hello, you've been selected! Please submit an update. Update is due by 6pm.",
+        member.about.id
+      )
+    );
+    reminders.push(
+      sendMsg(
+        'Na, ez a beszéd!!!!! A kovetkezo ember aki remeljuk fog irni: ' +
+          member.about.first_name +
+          ' (@' +
+          member.about.username +
+          '):',
+        group.chatId
+      )
+    );
   });
 
   await Promise.all(reminders);
